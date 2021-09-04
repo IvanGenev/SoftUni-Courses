e6594ef7e14b6f828512a61560fd1197e853ad9f
IvanGenev <ivangenev@gmail.com>
2021-09-04 19:53:45 +0300

    Added new Lab

---
+++ C#-Advanced-Old/MultidimensionalArraysLab/InitializingMatrixWithValues/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InitializingMatrixWithValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                {2,3,4 },
+                {4,6,7 },
+                {3,4,5 }
+            };
+
+            // Foreach iterates through all elements in the matrix:
+
+            //foreach (int element in matrix)
+            //{
+            //    Console.WriteLine(element);
+            //}
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write("{0} ", matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#-Advanced-Old/MultidimensionalArraysLab/ReadingMatrixFromConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ReadingMatrixFromConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = ReadArrayFromConsole();
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            for (int row = 0; row < sizes[0]; row++)
+            {
+                int[] colElements = ReadArrayFromConsole();
+
+                for (int col = 0; col < sizes[1]; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+        }
+
+        private static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
